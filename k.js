6148bf8a6802a63b51c85fc6976b71e49d438aa1
Kenjesan2023 <143146547+Kenjesan2023@users.noreply.github.com>
2023-09-17 12:02:59 +0800

    Create k.js

---
+++ k.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Function to be executed when the page loads
+function onPageLoad() {
+    alert("View my Resume?");
+}
+
+// Add an event listener to call onPageLoad when the page finishes loading
+window.addEventListener("load", onPageLoad);
